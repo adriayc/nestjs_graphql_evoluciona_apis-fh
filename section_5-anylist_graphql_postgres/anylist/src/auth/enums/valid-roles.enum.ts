07fcd321406e71c00ba43a6129ac33a0a72ba353
Adriano Ayala <adriano.ayc@gmail.com>
2026-06-26 16:40:51 -0400

    GraphQL Enumerations

--- section_5-anylist_graphql_postgres/anylist/src/auth/enums/valid-roles.enum.ts
+++ section_5-anylist_graphql_postgres/anylist/src/auth/enums/valid-roles.enum.ts
@@ -1,6 +1,13 @@
-// TODO: Implementar enum como GraphQL Enum Type
+import { registerEnumType } from '@nestjs/graphql';
+
 export enum ValidRoles {
   admin = 'admin',
   user = 'user',
   superUser = 'superUser',
 }
+
+// Resgiste enum type on GraphQL
+registerEnumType(ValidRoles, {
+  name: 'ValidRoles',
+  description: `Roles permitidos [${ValidRoles.admin}, ${ValidRoles.user}, ${ValidRoles.superUser}] para la enumeración`,
+});
