94e46b0464138aaa0de85cf7c48e110d44fe8051
Adriano Ayala <adriano.ayc@gmail.com>
2026-06-29 16:39:28 -0400

    Seed - Paso 2: Borrar registros anteriores

--- section_5-anylist_graphql_postgres/anylist/src/seed/seed.module.ts
+++ section_5-anylist_graphql_postgres/anylist/src/seed/seed.module.ts
@@ -3,12 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 
 import { SeedService } from './seed.service';
 import { SeedResolver } from './seed.resolver';
+import { ItemsModule } from 'src/items/items.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   providers: [SeedResolver, SeedService],
   imports: [
     // Config module (env)
     ConfigModule,
+    // Users module
+    UsersModule,
+    // Items module
+    ItemsModule,
   ],
 })
 export class SeedModule {}
--- section_5-anylist_graphql_postgres/anylist/src/seed/seed.service.ts
+++ section_5-anylist_graphql_postgres/anylist/src/seed/seed.service.ts
@@ -1,11 +1,23 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Item } from 'src/items/entities/item.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class SeedService {
   private isProd: boolean;
 
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>,
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {
     this.isProd = configService.get('NEST_ENV') === 'prod';
   }
 
@@ -16,9 +28,25 @@ export class SeedService {
     }
 
     // Limpiar la DB
+    await this.deleteDB();
     // Crear users
     // Crear items
 
     return true;
+  }
+
+  async deleteDB() {
+    // Delete items
+    await this.itemsRepository
+      .createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
+    // Delete users
+    await this.usersRepository
+      .createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
   }
 }
