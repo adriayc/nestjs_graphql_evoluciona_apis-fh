2b835e3db74229ddfa6d06bfb746960ea0333ac2
Adriano Ayala <adriano.ayc@gmail.com>
2026-06-29 16:20:14 -0400

    Seed - Paso 1: Protección

--- section_5-anylist_graphql_postgres/anylist/src/seed/seed.module.ts
+++ section_5-anylist_graphql_postgres/anylist/src/seed/seed.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { SeedService } from './seed.service';
 import { SeedResolver } from './seed.resolver';
 
 @Module({
   providers: [SeedResolver, SeedService],
+  imports: [
+    // Config module (env)
+    ConfigModule,
+  ],
 })
 export class SeedModule {}
--- section_5-anylist_graphql_postgres/anylist/src/seed/seed.service.ts
+++ section_5-anylist_graphql_postgres/anylist/src/seed/seed.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SeedService {
+  private isProd: boolean;
+
+  constructor(private readonly configService: ConfigService) {
+    this.isProd = configService.get('NEST_ENV') === 'prod';
+  }
+
   async executeSeed(): Promise<boolean> {
+    // Validate environment
+    if (this.isProd) {
+      throw new UnauthorizedException('We cannot run SEED on Prod environment');
+    }
+
     // Limpiar la DB
     // Crear users
     // Crear items
