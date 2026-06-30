f34492e5a7ebf47f1a1c9af19841ccef5fa5cf01
Adriano Ayala <adriano.ayc@gmail.com>
2026-06-30 16:43:18 -0400

    Múltiples Args - Búsquedas por nombre

--- section_5-anylist_graphql_postgres/anylist/src/items/items.resolver.ts
+++ section_5-anylist_graphql_postgres/anylist/src/items/items.resolver.ts
@@ -7,7 +7,7 @@ import { CreateItemInput, UpdateItemInput } from './dto/inputs';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { User } from 'src/users/entities/user.entity';
-import { PaginationArgs } from 'src/common/dto/args/pagination.args';
+import { PaginationArgs, SearchArgs } from 'src/common/dto/args';
 
 @Resolver(() => Item)
 @UseGuards(JwtAuthGuard)
@@ -26,6 +26,7 @@ export class ItemsResolver {
   async findAll(
     @CurrentUser() user: User,
     @Args() paginationArgs: PaginationArgs,
+    @Args({ nullable: true }) searchArgs?: SearchArgs,
   ): Promise<Item[]> {
     return await this.itemsService.findAll(user, paginationArgs);
   }
